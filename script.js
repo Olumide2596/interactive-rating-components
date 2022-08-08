b61fb0766c1df24bf41dafa05ad43ecdda0046c3
Olumide2596 <olumide2596@gmail.com>
2022-08-08 12:28:13 +0100

    almost done

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 
 const submitBtn = document.querySelector('.submit');
 const thankYou = document.querySelector('.thank-you');
+const rateUs = document.querySelector('.rate-us');
+const circles = document.querySelectorAll('.circle');
+const rating = document.querySelector('.rating');
+
+circles.forEach((circle, i) =>
+	circle.addEventListener('click', () => {
+		// circle.classList.reomve('active');
+		circle.classList.toggle('active');
+		rating.textContent = `${i + 1}`;
+	})
+);
+
+submitBtn.addEventListener('click', function () {
+	rateUs.classList.add('disable');
+	thankYou.classList.remove('disable');
+});
