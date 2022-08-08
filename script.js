5a38a52aeae21fdff941d3df34c9777bad9bdbe4
Olumide2596 <olumide2596@gmail.com>
2022-08-08 22:46:30 +0100

    commit

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ const thankYou = document.querySelector('.thank-you');
 const rateUs = document.querySelector('.rate-us');
 const circles = document.querySelectorAll('.circle');
 const rating = document.querySelector('.rating');
+const submitText = document.querySelector('.submit-p');
 
 circles.forEach((circle, i) =>
 	circle.addEventListener('click', () => {
-		// circle.classList.reomve('active');
-		circle.classList.toggle('active');
+		// circle.classList.toggle('active');
+		// used :focu on the css to solve
 		rating.textContent = `${i + 1}`;
 	})
 );
 
 submitBtn.addEventListener('click', function () {
-	rateUs.classList.add('disable');
-	thankYou.classList.remove('disable');
+	if (rating.textContent) {
+		rateUs.classList.add('disable');
+		thankYou.classList.remove('disable');
+	} else {
+		submitText.textContent = `Choose a rating😁`;
+	}
 });
